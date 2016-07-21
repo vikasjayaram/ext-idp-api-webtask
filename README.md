f9a41c1365f80f12ee2f28b3864725a272082999
Vikas Kannurpatti Jayaram <vikas@auth0.com>
2016-07-21 20:36:24 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Calling A External IDP API Eg. Google using webtask
+
+Sample project for creating an Express-based server that runs on webtask.io for accessing external IDP api with the user's idp access token.
+### Version
+0.0.1
+# Initial Setup & Configuration
+```bash
+# Create a new wt-cli profile
+npm install -g wt-cli
+wt init
+
+# Or, if you already use wt-cli:
+wt profile ls
+```
+
+Please read the following link on how to
+[Call an Identity Provider API ](https://auth0.com/docs/what-to-do-once-the-user-is-logged-in/calling-an-external-idp-api)
+
+Some more resources to read on [Identity Provider Access Tokens](https://auth0.com/docs/tokens/idp)
+
+### Initialization
+```sh
+$ wt create ext_idp_webtask.js
+    -s CLIENT_ID=YOUR_NON_INTERACTIVE_AUTH0_CLIENT_ID
+    -s CLIENT_SECRET=YOUR_NON_INTERACTIVE_AUTHO_CLIENT_SECRET
+    -s ACCOUNT_NAME=YOUR_AUTH0_TENANT_NAME
+    -s ID_TOKEN_CLIENT_SECRET=YOUR_CLIENT_SECRET
+```
+The above command would create a webtask and give you a url like this
+```
+Webtask created
+
+You can access your webtask at the following url:
+
+https://webtask.it.auth0.com/api/run/wt-vikas_ramasethu-gmail_com-0/ext_idp_webtask/call_ext_api?webtask_no_cache=1
+```
+# Usage
+```sh
+  const options = {
+    url: 'URL_WHEN_YOU_CREATE_WEBTASK',
+    headers: {Authorization: 'Bearer USER_ID_TOKEN'},
+    json: {api_url: 'IDP_API'}
+  }
+  request.post(options, function (e, r, b){});
+```
+# Example Usage
+
+The below is a sample on how to call a Google API with.
+
+```sh
+const options = {
+    url: 'https://webtask.it.auth0.com/api/run/wt-vikas_ramasethu-gmail_com-0/ext_idp_webtask/call_ext_api?webtask_no_cache=1',
+    headers: {Authorization: 'Bearer USER_ID_TOKEN'},
+    json: {api_url: 'https://www.googleapis.com/youtube/v3/channels?part=snippet%2C+id%2C+statistics&mine=true'}
+  }
+  request.post(options, function (e, r, b){});
+```
---
+++ ext_idp_webtask.js
@@ -0,0 +1,119 @@
+"use strict";
+
+ const jwt     = require('jsonwebtoken');
+ const moment  = require('moment');
+ const request = require('request');
+ const express = require('express');
+ const Webtask = require('webtask-tools');
+ const app     = express();
+ const async = require('async');
+
+ /*
+ * Local variables
+ */
+ let accessToken = null;
+ let lastLogin = null;
+
+ app.post('/call_ext_api', function (req, res) {
+   if (!req.headers['authorization']){ return res.status(401).json({ error: 'unauthorized'}); }
+   if (req.webtaskContext.body === undefined) {return res.status(400).json({error: 'api_url is required'}); }
+   if (!req.webtaskContext.body || !req.webtaskContext.body['api_url']){return res.status(400).json({error: 'api_url is required'}); }
+   const context = req.webtaskContext;
+   const token = req.headers['authorization'].split(' ')[1];
+   const reqBody = req.webtaskContext.body;
+   console.log(reqBody);
+   async.waterfall([
+     async.apply(verifyJWT, context, reqBody, token),
+     getAccessToken,
+     getUserProfile,
+     callExtIDPApi
+   ], function (err, result) {
+     if (err) return res.status(400).json({error: err});
+     return res.status(200).json({data: result});
+   });
+ });
+/*
+* Verify that the user id_token is signed by the correct Auth0 client
+*/
+function verifyJWT(context, reqBody, token, cb) {
+   return jwt.verify(token, new Buffer(context.data.ID_TOKEN_CLIENT_SECRET, 'base64'), function(err, decoded) {
+     if (err) return cb(err);
+     cb(null, context, reqBody, decoded);
+   });
+};
+/*
+* Request a Auth0 access token every 24 hours
+*/
+function getAccessToken(context, reqBody, decoded, cb) {
+   if (!accessToken || !lastLogin || moment(new Date()).diff(lastLogin, 'minutes') > 1440) {
+     const options = {
+       url: 'https://' + context.data.ACCOUNT_NAME + '.auth0.com/oauth/token',
+       json: {
+         audience: 'https://' + context.data.ACCOUNT_NAME + '.auth0.com/api/v2/',
+         grant_type: 'client_credentials',
+         client_id: context.data.CLIENT_ID,
+         client_secret: context.data.CLIENT_SECRET
+       }
+     };
+
+     return request.post(options, function(err, response, body){
+       if (err) return cb(err);
+       else {
+         lastLogin = moment();
+         accessToken = body.access_token;
+         return cb(null, context, reqBody, decoded, accessToken);
+       }
+     });
+   } else {
+     return cb(null, context, reqBody, decoded, accessToken);
+   }
+ };
+
+/*
+* Get the complete user profile with the read:user_idp_token scope
+*/
+function getUserProfile(context, reqBody, decoded, token, cb){
+   const options = {
+     url: 'https://' + context.data.ACCOUNT_NAME + '.auth0.com/api/v2/users/' + decoded.sub,
+     json: true,
+     headers: {
+       authorization: 'Bearer ' + token
+     }
+   };
+
+  request.get(options, function(error, response, user){
+     return cb(error, context, reqBody, user);
+   });
+ };
+
+/*
+* Call the External API with the IDP access token to return data back to the client.
+*/
+function callExtIDPApi (context, reqBody, user, cb) {
+  let idp_access_token = null;
+  const api = reqBody.api_url;
+  const provider = user.user_id.split('|')[0];
+  if (user && user.identities) {
+    for (var i = 0; i < user.identities.length; i++) {
+      if (user.identities[i].access_token && user.identities[i].provider === provider) {
+        idp_access_token = user.identities[i].access_token;
+        i = user.identities.length;
+      }
+    }
+  }
+  if (idp_access_token) {
+    var options = {
+      method: 'GET',
+      url: api,
+      headers: {Authorization: 'Bearer ' + idp_access_token}
+    };
+    request(options, function (error, response, body) {
+      if (error) cb(error);
+      cb(null, JSON.parse(body));
+    });
+  } else {
+    cb('No Access Token Available');
+  }
+};
+
+module.exports = Webtask.fromExpress(app);
